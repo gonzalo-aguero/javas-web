eaeac9274bff2a57e11e9860f3692647259c0b94
Gonzalo Aguero <gonzalo90fa@gmail.com>
2021-09-09 23:49:11 -0300

    aboutUs and ourServices

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,11 +7,13 @@
             <img src="{{asset('img/design/coverpage.jpg')}}">
             <span></span>
         </div>
-        <svg width="1000" height="1000" class="decorationBlock i0">
-            <polygon points="0,0 1000,0 1000,1000" stroke="none" stroke-width="0" fill="var(--color1)"></polygon>
+        <!-- White triangle -->
+        <svg width="740" height="500" class="decorationBlock i0">
+            <polygon points="0,0 740,0 740,500" stroke="none" stroke-width="0" fill="var(--color1)"></polygon>
         </svg>
-        <svg width="1000" height="1000" class="decorationBlock i1">
-            <polygon points="1000,0 1000,1000 0,1000" stroke="none" stroke-width="0" fill="var(--color4)"></polygon>
+        <!-- Yellow Triangle -->
+        <svg width="500" height="500" class="decorationBlock i1">
+            <polygon points="500,0 500,500 0,500" stroke="none" stroke-width="0" fill="var(--color4)"></polygon>
         </svg>
         <div class="text">
             <h1>JAVAS<br>WEB</h1>
@@ -20,8 +22,8 @@
     </section>
     <section id="aboutUs">
         <div class="image" style="background-image: url({{asset('img/design/aboutUs.jpg')}})"></div>
-        <svg width="1000" height="1000" class="decorationBlock i1">
-            <polygon points="0,0 1000,0 500,500" stroke="none" stroke-width="0" fill="var(--color4)"></polygon>
+        <svg width="600" height="300" class="decorationBlock i1">
+            <polygon points="0,0 600,0 300,300" stroke="none" stroke-width="0" fill="var(--color4)"></polygon>
         </svg>
         <div class="text">
             <h1>INFORMACIÓN SOBRE LA EMPRESA</h1>
@@ -30,6 +32,34 @@
                 Buscamos ofrecer a nuestros clientes el mejor servicio, pensando siempre
                 en la mejor calidad a cambio del mejor precio.
             </p>
+        </div>
+    </section>
+    <section id="ourServices">
+        <!-- Black triangle -->
+        <svg width="300" height="300" class="decorationBlock i0">
+            <polygon points="0,0 300,300 0,300" stroke="none" stroke-width="0" fill="var(--color6)"></polygon>
+        </svg>
+        <!-- Yellow triangle -->
+        <svg width="300" height="300" class="decorationBlock i1">
+            <polygon points="0,0 300,300 300,0" stroke="none" stroke-width="0" fill="var(--color4)"></polygon>
+        </svg>
+        <h2>NUESTROS SERVICIOS</h2>
+        <div class="services">
+            <div class="service i0">
+                <img src="{{asset('img/icons/ourServicesIcon1.png')}}" alt="">
+                <h3>CONSULTORÍA INFORMÁTICA</h3>
+                <p>Te ayudamos a maximizar tu red y procesos informáticos.</p>
+            </div>
+            <div class="service i1">
+                <img src="{{asset('img/icons/ourServicesIcon2.png')}}" alt="">
+                <h3>SOLUCIONES EN LA NUBE</h3>
+                <p>Conservá los datos de tus negocios y aplicaciones de manera segura.</p>
+            </div>
+            <div class="service i2">
+                <img src="{{asset('img/icons/ourServicesIcon3.png')}}" alt="">
+                <h3>CONTINUIDAD EMPRESARIAL</h3>
+                <p>¡Volvé a recuperar tus datos cuando ocurra un desastre!</p>
+            </div>
         </div>
     </section>
 @endsection
